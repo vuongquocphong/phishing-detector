aff10d5368095cb1dc0e1e50ce7afe2f7e0061ac
Locopaly <95960924+Locopaly@users.noreply.github.com>
2024-09-14 00:14:01 +0700

    Update model_api.py

--- model_api.py
+++ model_api.py
@@ -1,3 +1,4 @@
+import subprocess
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import torch
@@ -22,11 +23,34 @@ def predict_phishing():
     print(f"Received data: {data}")
     email_content = data['email_content']
     urls = data['urls']  # Extract URLs from the request
+    malicious_diff = 0
+
+    # Write the URLs to a file
+    with open("test_urls.txt", "w", encoding="utf-8") as f:
+        for url in urls:
+            f.write("-1" + "\t" + url + "\n")
+    
+    # Run the URL scanner
+    subprocess.run(["python", "test.py"], check=True)
+
+    # Read the output of the URL scanner
+    with open("output/test_results.txt", "r", encoding="utf-8") as f:
+        url_results = f.readlines()
+        url_results = url_results[1:]
+        for line in url_results:
+            _, predict, _ = line.split("\t")
+            if predict == "1":
+                malicious_diff += 1
+            else:
+                malicious_diff -= 1
     
     # Tokenize the input email content
     inputs = tokenizer(email_content, return_tensors='pt', truncation=True, padding=True, max_length=512)
 
     inputs.to(device)
+
+    global model
+    model = model.to(device)
 
     # Set the model to evaluation mode
     model.eval()
@@ -41,7 +65,7 @@ def predict_phishing():
     # Get the predicted class
     prediction = torch.argmax(probs, dim=1)
     # Assuming the model outputs 1 for "Phishing" and 0 for "Safe"
-    result = "Phishing" if prediction == 1 else "Safe"
+    result = "Phishing" if (prediction == 1 or malicious_diff > 0) else "Safe"
 
     print("--------------------")
 
